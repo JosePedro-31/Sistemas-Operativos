f5e651fb84057ea21c4580cc1578da10bd0df193
josepedro-31 <jose.pedro.7966@gmail.com>
2024-03-13 19:43:09 +0000

    alterações na forma como o servidor guarda a informação vinda do fifo;  correção na função exec; cálculo do tempo de execução

--- orchestrator.c
+++ orchestrator.c
@@ -9,11 +9,12 @@
 #include <errno.h>
 #include <time.h>
 
-typedef struct tarefas{
+typedef struct ListaTarefas{
     int tempo;
     char prog[20];
-    char args[100];
-} Tarefa;
+    char *args[300];
+    time_t tempo_total;
+} taskList;
 
 int main(int argc, char *argv[]) {
 
@@ -39,7 +40,7 @@ int main(int argc, char *argv[]) {
         close(servidor_cliente);
         
         // Escrever no ecrá que a tarefa foi recebida
-        int task = 0;
+        int task = 1;
         printf("TASK %d Received\n", task);
         task+=1;
 
@@ -48,28 +49,51 @@ int main(int argc, char *argv[]) {
         gettimeofday(&tempoinit, NULL);
         time_t tempo_inicial = tempoinit.tv_sec;
 
-        // int tempo;
-        // char prog[20];
-        // char args[100];
-        // sscanf(buffer, "%d %s %s", &tempo, prog, args);
-
-        // separar o buffer usando o strtok
-        char *tempo_str = strtok(buffer, " ");
-        char *prog = strtok(NULL, " ");
-        char *args = strtok(NULL, " ");
-
+        // separar os elementos do buffer usando a strtok para uma Tarefa
+        taskList tarefa;
+        char *token = strtok(buffer, " ");
         // converter o tempo para inteiro
-        int tempo = atoi(tempo_str);
-
+        tarefa.tempo = atoi(token);
+        token = strtok(NULL, " ");
+        // copiar o prog para a struct
+        strcpy(tarefa.prog, token); 
+        token = strtok(NULL, " ");
+        // copiar os args para a struct 
+        int i = 0;
+        // enquanto houver tokens
+        while (token != NULL) {
+            // copiar o token para a struct
+            tarefa.args[i] = token;
+            // ir buscar o próximo token
+            token = strtok(NULL, " ");
+            i++;
+        }
         
         // criar um processo filho
         pid_t pid = fork();
-
         if (pid == 0) {
-            // executar o programa
-            execl(prog, args, NULL);
+            // executar o prog com os args
+            execvp(tarefa.prog, tarefa.args);
+            // se o execvp falhar
+            perror("Erro na execução do prog\n");
             _exit(1);
         }
+        else {
+            // PROCESSO PAI
+            // esperar que o processo filho termine
+            int status;
+            int terminated_pid;
+            terminated_pid = wait(&status);
+            if (WIFEXITED(status)) {
+                // registar o tempo através da função gettimeofday
+                struct timeval tempofinal;
+                gettimeofday(&tempofinal, NULL);
+                time_t tempo_final = tempofinal.tv_sec;
+                // calcular o tempo que demorou a tarefa
+                tarefa.tempo_total = tempo_final - tempo_inicial;
+            }
+        }    
+
     }
 
     return 0;
